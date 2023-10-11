61a7ee4cac4c2906a506268c95b8b1528ad2511c
mahadi570 <mahadihasan18407@gmail.com>
2023-10-11 17:03:07 +0400

    empty todo fixed

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.querySelector('#add-btn').addEventListener('click', function(e){
     const textArea = document.querySelector('#text-area');
-    
-
-    const li = document.createElement('li');
+    if(textArea.value.length > 0){
+        const li = document.createElement('li');
     li.innerText = textArea.value;
     li.classList.add('text-2xl');
 
     document.querySelector('#to-do-container').appendChild(li);
     textArea.value = '';
-    
-})
+    }
+    else{
+        alert("<Write something first...")
+    }
+    })
